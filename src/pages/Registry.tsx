69257320c6e601cb87d495dd85d785744c7151e5
jude.edwards <jude.edwards@buildertrend.com>
2025-03-22 14:01:10 -0500

    c

--- src/pages/Registry.tsx
+++ src/pages/Registry.tsx
@@ -34,7 +34,7 @@ const Registry = () => {
             <img alt="Gift Registry" className="w-full h-[250px] object-cover mb-6 grayscale" src="/lovable-uploads/4a1b2244-0f3c-4993-b643-aac101abba5b.jpg" />
             <h2 className="text-2xl font-serif mb-4">GIFT FOR THE COUPLE</h2>
             <p className="mb-6">
-              Help us build our dream home <br />
+              Help us build our dream home <br /> <br />
               Zelle: 385-900-6425. Jude Edwards.
             </p>
             <Link to="#" className="btn">LEARN MORE</Link>
@@ -53,8 +53,9 @@ const Registry = () => {
             <img alt="Photo Book" src="/lovable-uploads/17bd2496-f49f-49ab-b31a-cb05d7c1e177.jpg" className="w-full h-[250px] object-cover mb-6" />
             <h2 className="text-2xl font-serif mb-4">HONEYMOON FUND</h2>
             <p className="mb-6">
-            Contribute to our experiences and adventures as a married couple. <br />
-            Apple Pay : 385-900-6425. Jude Edwards. or Zelle : 385-900-6425. Jude Edwards.
+            Contribute to our experiences and adventures as a married couple. <br /> <br />
+            Apple Pay : 385-900-6425. Jude Edwards. <br />
+             Zelle : 385-900-6425. Jude Edwards.
             </p>
             <Link to="#" className="btn">LEARN MORE</Link>
           </motion.div>
